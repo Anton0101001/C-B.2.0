1f8cbaeb6fd4e63096efbd8c2b29cc2f5294f2cb
anton shabanov <89005421555@mail.ru>
2022-06-03 11:08:14 +0300

    add junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,53 +1,53 @@
 package ru.netology.service;
 
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 
 public class CashbackHackServiceTest {
 
     @Test
-    void Cashback1() {
+   public void Cashback1() {
         int amount = 500;
         int expected = 500;
 
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void Cashback2() {
+   public void Cashback2() {
         int amount = 900;
         int expected = 100;
 
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
 
     @Test
-    void Cashback3() {
+    public void Cashback3() {
         int amount = 100;
         int expected = 900;
 
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
 
     @Test
-    void Cashback4() {
+    public void Cashback4() {
         int amount = 1000;
         int expected = 0;
 
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
 }
